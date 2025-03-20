f681a8e680ad099b7c993b09e8c02d7c4055dde0
mikami koji <wmd147wmd@gmail.com>
2025-03-21 08:34:39 +0900

    ヘッターのレイアウト整形

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Noto_Sans_JP } from 'next/font/google';
 import './globals.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const notoSansJp = Noto_Sans_JP({
   subsets: ['latin'],
@@ -23,7 +25,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={notoSansJp.variable}>{children}</body>
+      <body className={notoSansJp.variable}>
+        <div className="min-h-screen content-wrapper px-4 text-sm">
+          <header className="pt-8 pb-6">
+            <div className="flex items-end justify-between">
+              <h1 className="text-h1">ペット体調記録アプリ</h1>
+              <div className="flex items-center gap-8">
+                <Link href="/login" className="flex items-center gap-1">
+                  <Image
+                    src="images/icons/record_list_icon.svg"
+                    alt="記録一覧"
+                    width={16}
+                    height={16}
+                  />
+                  記録一覧
+                </Link>
+                <Link href="/login" className="flex items-center gap-1">
+                  <Image
+                    src="images/icons/setting_icon.svg"
+                    alt="設定"
+                    width={16}
+                    height={16}
+                  />
+                  設定
+                </Link>
+              </div>
+            </div>
+          </header>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
-    <div>
-      <p>テスト</p>
-      <p>tes</p>
+    <div className="contentWrapper">
+      <Button variant="black">記録ボタン</Button>
+      <div className="newRecords">
+        <h2>最新の記録</h2>
+        <ul className="petNamesList">
+          <li className="petNameItem">
+            <p className="petName">ペットの名前</p>
+          </li>
+        </ul>
+        <ul className="recordsList">
+          <li className="recordsItem">
+            <ul className="recordList">
+              <li className="listItem">記録情報</li>
+              <li className="listItem">記録情報</li>
+              <li className="listItem">記録情報</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
